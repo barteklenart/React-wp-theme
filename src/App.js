fb4fb9effe8cd1a976357917905743b7694fa7c8
barteklenart <bartlomiejlenartbl@gmail.com>
2017-09-25 20:38:11 +0200

    list of posts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      posts: []
+    }
+  }
+
+  componentDidMount(){
+    axios.get( 'http://localhost:8080/reactwp/wp-json/wp/v2/posts' )
+      .then ( response => {
+        this.setState({
+          posts: response.data
+        })
+      })
+      .catch( error => {
+        console.log( error );
+      } )
+  }
+
   render() {
+    console.log( this.state );
+    const listOfPost = this.state.posts.map( post => {
+      return post.title.rendered;
+    })
+
     return (
       <div>
-        
+        { listOfPost }
       </div>
     );
   }
